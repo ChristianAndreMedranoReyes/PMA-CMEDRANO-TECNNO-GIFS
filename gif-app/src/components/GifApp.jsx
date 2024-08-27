cb80dcf2349a8ff97af6ed2a12a4409ad10b5be8
ChrisMedranoKinal <cmedrano-2020388@kinal.edu.gt>
2024-08-27 11:48:10 -0600

    pma

---
+++ gif-app/src/components/GifApp.jsx
@@ -0,0 +1,22 @@
+
+import { useGif } from "../hooks/useGif"
+ 
+export const GifApp = () => {
+ 
+    const {gif, handdleGif} = useGif()
+ 
+    return (
+    <>
+        <div className="container">
+            <div className="d-flex flex-row justify-content-center alig-items-center mt-3">
+                <button onClick={handdleGif} className="btn btn-outline-success">Generar Gif</button>
+            </div>
+            <div className="d-flex flex-row justify-content-center alig-items-center">
+                <img className="w-50 m-5" src= {gif} alt="image"/>
+            </div>
+        </div>
+    </>
+  )
+}
+
+
